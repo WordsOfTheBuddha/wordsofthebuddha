f2019eaca9436a111df95f6cf5ec19cc9329c070
Siddharth Kothari <siddharthlatest@gmail.com>
2024-09-23 20:25:13 +0700

    add (SN): 45.63, 64-68 - accomplishment in good friendship
    - accomplishment in virtue, aspiration, self-development, view,
      diligence

--- packages/website/components/NikayaTable.js
+++ packages/website/components/NikayaTable.js
@@ -5,7 +5,7 @@ import styles from "/styles/NikayaTable.module.css";
 const fallbackTranslationsCounts = {
   "sn": {
     "label": "Linked Discourses (SN)",
-    "translationCount": 170
+    "translationCount": 176
   },
   "an": {
     "label": "Numbered Discourses (AN)",
