12e5b8d46405700c3d90ae846f7da260d91dd994
Siddharth Kothari <sids.aquarius@gmail.com>
2025-02-13 23:38:16 +0530

    refactor (content): use collection layout for better reuse
    add filtering support and descriptions on all collection pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,41 +8,46 @@ import vercel from "@astrojs/vercel";
 import rehypeExternalLinks from "rehype-external-links";
 
 const externalLinksOptions = {
-  target: "_blank",
-  rel: ["noopener", "noreferrer"],
-  content: { type: "text", value: " ↗" },
+	target: "_blank",
+	rel: ["noopener", "noreferrer"],
+	content: { type: "text", value: " ↗" },
 };
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    rehypePlugins: [[rehypeExternalLinks, externalLinksOptions]],
-  },
+	markdown: {
+		rehypePlugins: [[rehypeExternalLinks, externalLinksOptions]],
+	},
 
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    mdx({
-      rehypePlugins: [[rehypeExternalLinks, externalLinksOptions]],
-    }),
-  ],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		mdx({
+			rehypePlugins: [[rehypeExternalLinks, externalLinksOptions]],
+		}),
+	],
 
-  vite: {
-    optimizeDeps: {
-      include: ["rangy"],
-    },
-    build: {
-      commonjsOptions: {
-        include: [/rangy/],
-      },
-    },
-    ssr: {
-      noExternal: ["rangy"],
-    },
-  },
+	vite: {
+		optimizeDeps: {
+			include: ["rangy"],
+		},
+		build: {
+			commonjsOptions: {
+				include: [/rangy/],
+			},
+		},
+		ssr: {
+			noExternal: ["rangy"],
+		},
+	},
 
-  adapter: vercel({
-    maxDuration: 30,
-  }),
+	adapter: vercel({
+		maxDuration: 30,
+	}),
+
+	redirects: {
+		"/dhammapada": "/dhp",
+		"/suttanipata": "/snp",
+	},
 });
